8f44d80f8ef44f2460c9d2b1e5e5fb4379d1ab25
Robert Mills <padi162070@hotmail.com>
2024-12-06 12:30:51 -0800

    adding Functions

---
+++ Functions/udf_R_EntityPhone.sql
@@ -0,0 +1,16 @@
+﻿CREATE FUNCTION dbo.udf_R_EntityPhone
+(@EntityId int)
+RETURNS varchar(100)
+AS
+BEGIN
+RETURN(
+	SELECT 
+		TOP 1 P.Phone 
+	FROM 
+		dbo.Phones AS P 
+	WHERE 
+		P.EntityID = @EntityId
+	AND P.IsPrimary = 0x01
+	ORDER BY
+		P.ContactTypeID DESC);
+END
