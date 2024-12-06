7a9236d76618e71a3bc3f76d5dc95c0735d94a02
Robert C Mills <136384613+RCMills@users.noreply.github.com>
2024-12-06 11:50:10 -0800

    Add files via upload

---
+++ vw_CaseLogSearch.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW dbo.vw_CaseLogSearch WITH SCHEMABINDING
+AS
+	SELECT
+		CL.CaseLogId
+		,Cl.FirstName
+		,Cl.LastName
+		,Cl.CustomerEmail
+		,CONCAT(FirstName, ' ', LastName, ' ', CustomerEmail) as SearchField
+	FROM
+		[case].[CaseLog] CL
+GO
+CREATE UNIQUE CLUSTERED INDEX [ClusteredIndex_vw_CaseLogSearch_CaseLogId]
+    ON [dbo].[vw_CaseLogSearch]([CaseLogId] ASC);
+
---
+++ vw_OfficeSyncUpdate.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW dbo.vw_OfficeSyncUpdate
+AS
+
+SELECT
+	IOS.*,
+	I.EntityId [IndividualEntityId],
+	O.EntityId [OfficeEntityId]
+FROM
+	staging.IndividualOfficeSync IOS
+LEFT OUTER JOIN dbo.Individual I ON
+	I.FlexId = IOS.AgentId
+LEFT OUTER JOIN dbo.Office O ON
+	O.OfficeId = IOS.OfficeId;
